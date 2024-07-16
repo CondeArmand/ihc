fa6e1c1d1eca09e5e034d8aa66a4c5c9c2d85134
CondeArmand <neoarmandosousa1010@hotmail.com>
2024-07-16 11:27:02 -0300

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../header/header.component';
-import { PresentationComponent } from '../presentation/presentation.component';
+import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { DetailsComponent } from './details/details.component';
+import { FooterComponent } from './footer/footer.component';
+import { FooterModule } from '@coreui/angular';
 
 @NgModule({
   declarations: [
     AppComponent,
       HeaderComponent,
       HeaderComponent,
-      PresentationComponent
+      PresentationComponent,
+      DetailsComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
